81952497470b8ad373543e91c6184d1c8e8ed494
Jeremy Redburn <jredburn@salsify.com>
2013-03-29 15:36:09 -0400

    Adding ability to acquire & release lock outside block context

--- lib/with_advisory_lock/concern.rb
+++ lib/with_advisory_lock/concern.rb
@@ -17,20 +17,48 @@ module WithAdvisoryLock
       self.class.with_advisory_lock(lock_name, timeout_seconds, &block)
     end
 
+    def acquire_advisory_lock(lock_name)
+      self.class.acquire_advisory_lock(lock_name)
+    end
+
+    def release_advisory_lock(lock_name)
+      self.class.release_advisory_lock(lock_name)
+    end
+
     module ClassMethods
 
       def with_advisory_lock(lock_name, timeout_seconds=nil, &block)
-        impl_class = case (connection.adapter_name.downcase)
-          when "postgresql"
-            WithAdvisoryLock::PostgreSQL
-          when "mysql", "mysql2"
-            WithAdvisoryLock::MySQL
-          else
-            WithAdvisoryLock::Flock
-        end
         impl = impl_class.new(connection, lock_name, timeout_seconds)
         impl.with_advisory_lock_if_needed(&block)
       end
+
+      def acquire_advisory_lock(lock_name)
+        impl = impl_class.new(connection, lock_name, nil)
+        res = impl.try_lock
+        puts "Acquire: #{res}"
+        res
+      end
+
+      def release_advisory_lock(lock_name)
+        impl = impl_class.new(connection, lock_name, nil)
+        res = impl.release_lock
+        puts "Release: #{res}"
+        res
+      end
+
+      private
+
+      def impl_class
+        case (connection.adapter_name.downcase)
+        when "postgresql"
+          WithAdvisoryLock::PostgreSQL
+        when "mysql", "mysql2"
+          WithAdvisoryLock::MySQL
+        else
+          WithAdvisoryLock::Flock
+        end
+      end
+
     end
   end
 end
--- test/simplest_test.rb
+++ test/simplest_test.rb
@@ -26,4 +26,40 @@ describe "simplest" do
     times.keys.must_equal [:t1_acquire, :t1_release, :t2_acquire, :t2_release]
     times[:t2_acquire].must_be :>, times[:t1_release]
   end
+
+  it "should prevent threads from accessing a resource concurrently in a non-block context" do
+    times = ActiveSupport::OrderedHash.new
+    ActiveRecord::Base.connection_pool.disconnect!
+    t1 = Thread.new do
+      ActiveRecord::Base.connection.reconnect!
+      while !ActiveRecord::Base.acquire_advisory_lock("simplest test") do
+        puts "Waiting on lock in t1"
+        sleep(rand * 0.15 + 0.05)
+      end
+      times[:t1_acquire] = Time.now
+      puts "Got lock in t1"
+      sleep 0.5
+      ActiveRecord::Base.release_advisory_lock("simplest test")
+      times[:t1_release] = Time.now
+      puts "Released lock in t1"
+    end
+    sleep 0.1
+    t2 = Thread.new do
+      ActiveRecord::Base.connection.reconnect!
+      while !ActiveRecord::Base.acquire_advisory_lock("simplest test") do
+        puts "Waiting on lock in t2"
+        sleep(rand * 0.15 + 0.05)
+      end
+      times[:t2_acquire] = Time.now
+      puts "Got lock in t2"
+      sleep 1
+      ActiveRecord::Base.release_advisory_lock("simplest test")
+      times[:t2_release] = Time.now
+      puts "Released lock in t2"
+    end
+    t1.join
+    t2.join
+    times.keys.must_equal [:t1_acquire, :t1_release, :t2_acquire, :t2_release]
+    times[:t2_acquire].must_be :>, times[:t1_release]
+  end
 end
